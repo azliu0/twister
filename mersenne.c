b7e8c0d03c602cbeb70ec9d93777a1f6d76190ad
Andrew Liu <andrewliu278@gmail.com>
2024-03-30 03:36:43 +0000

    add mtwister c impl

--- mersenne.c
+++ mersenne.c
@@ -3,12 +3,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "twister/mtwister.h"
+
 #define ANSI_BOLD       "\x1b[1m"
 #define ANSI_CLEAR      "\x1b[0m"
 #define ANSI_GREEN      "\x1b[32m"
 #define ANSI_RED        "\x1b[31m"
 
 uint64_t global_canary;
+
+void canary() {
+	printf("selecting canary\n");
+}
+
+void game() {
+	printf("playing game\n");
+}
 
 void start() {
 	char userAction;
@@ -25,10 +35,22 @@ void start() {
 	puts("  /_/`\"\\\\___");
 	puts("       `~~~`");
 	puts("");
-	printf("[" ANSI_GREEN "*" ANSI_CLEAR "] ");
+	printf("[" ANSI_GREEN "*" ANSI_CLEAR "]: ");
 	scanf(" %c", &userAction);
-
-	printf("You entered: %c\n", userAction);
+	
+	while(1) {
+		if (userAction == 'c') {
+			canary();
+			break;
+		} else if (userAction == 'p') {
+			game();
+			break;
+		} else {
+			printf("[" ANSI_GREEN "*" ANSI_CLEAR "] invalid action, please try again: ");
+		  while(getchar() != '\n');
+			scanf(" %c", &userAction);
+		}
+	}
 }
 
 void check_canary(uint64_t canary) {
@@ -41,10 +63,20 @@ void check_canary(uint64_t canary) {
 void generate_canary() {
 }
 
+void test_rand() {
+	MTRand r = seedRand(1337);
+	int i;
+	for (i=0; i < 1000; i++){
+		printf("%ld\n", genRandLong(&r));
+	}
+	return;
+}
+
 int main() {
 	gid_t gid = getegid();
 	setresgid(gid, gid, gid);
-
+	
+	test_rand();
 	start();
 
 	return 0;
