b6b7021e4c30e2e22cd79cb27c02401b31855bb5
Andrew Liu <andrewliu278@gmail.com>
2024-03-30 05:16:49 +0000

    implement canary select

--- challenge/mersenne.c
+++ challenge/mersenne.c
@@ -8,6 +8,7 @@
 
 #define ANSI_BOLD "\x1b[1m"
 #define ANSI_CLEAR "\x1b[0m"
+#define ANSI_BLUE "\x1b[34m"
 #define ANSI_GREEN "\x1b[32m"
 #define ANSI_RED "\x1b[31m"
 
@@ -35,7 +36,38 @@ void seed(MTRand *rng)
 
 void canary()
 {
-	printf("selecting canary\n");
+	puts("");
+	puts("      canary select");
+	puts("");
+
+	unsigned long canary = genRandLong(&rng);
+	char userAction;
+
+	printf("[" ANSI_BLUE "*" ANSI_CLEAR "] canary_id: %ld\n", canary);
+	printf("[" ANSI_GREEN "*" ANSI_CLEAR "] regenerate? (y/n) ");
+	scanf(" %c", &userAction);
+
+	while(1)
+	{
+		if (userAction == 'y')
+		{
+			canary = genRandLong(&rng);
+			printf("[" ANSI_BLUE "*" ANSI_CLEAR "] canary_id: %ld\n", canary);
+			printf("[" ANSI_GREEN "*" ANSI_CLEAR "] regenerate? (y/n) ");
+			while (getchar() != '\n');
+			scanf(" %c", &userAction);
+		}
+		else if (userAction == 'n')
+		{
+			return;
+		}
+		else
+		{
+			printf("[" ANSI_GREEN "*" ANSI_CLEAR "] invalid action, please try again: ");
+			while (getchar() != '\n');
+			scanf(" %c", &userAction);
+		}
+	}
 }
 
 void game()
@@ -43,10 +75,8 @@ void game()
 	printf("playing game\n");
 }
 
-void start()
-{
-	char userAction;
-
+void welcome()
+{	
 	puts("");
 	puts("       .-\"-.");
 	puts("      /  ,~a\\_");
@@ -55,11 +85,18 @@ void start()
 	puts("     /  (    \\");
 	puts("    /   )    ;     [c] canary select");
 	puts("   /   /     /     [p] play game");
-	puts(" ,/_.'`  _.-`");
+	puts(" ,/_.'`  _.-`      [e] exit");
 	puts("  /_/`\"\\\\___");
 	puts("       `~~~`");
 	puts("");
 	printf("[" ANSI_GREEN "*" ANSI_CLEAR "]: ");
+}
+
+void start()
+{
+	char userAction;
+
+	welcome();
 	scanf(" %c", &userAction);
 
 	while (1)
@@ -67,18 +104,23 @@ void start()
 		if (userAction == 'c')
 		{
 			canary();
-			break;
+			welcome();
+			while (getchar() != '\n');
+			scanf(" %c", &userAction);
 		}
 		else if (userAction == 'p')
 		{
 			game();
 			break;
 		}
+		else if (userAction == 'e')
+		{
+			break;
+		}
 		else
 		{
 			printf("[" ANSI_GREEN "*" ANSI_CLEAR "] invalid action, please try again: ");
-			while (getchar() != '\n')
-				;
+			while (getchar() != '\n');
 			scanf(" %c", &userAction);
 		}
 	}
@@ -93,28 +135,13 @@ void check_canary(uint64_t canary)
 	}
 }
 
-void generate_canary()
-{
-}
-
-void test_rand()
-{
-	int i;
-	for (i = 0; i < 1000; i++)
-	{
-		printf("%ld\n", genRandLong(&rng));
-	}
-	return;
-}
-
 int main()
 {
 	gid_t gid = getegid();
 	setresgid(gid, gid, gid);
 
 	seed(&rng);
-	test_rand();
-	// start();
+	start();
 
 	return 0;
 }
