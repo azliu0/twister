d25f4a22781c1c71be6ecad6fad70714114cc369
Andrew Liu <andrewliu278@gmail.com>
2024-03-29 23:35:46 -0500

    add dev/urandom seeding, and formatting

--- src/mersenne.c
+++ src/mersenne.c
@@ -5,24 +5,48 @@
 
 #include "mtwister.h"
 
-#define ANSI_BOLD       "\x1b[1m"
-#define ANSI_CLEAR      "\x1b[0m"
-#define ANSI_GREEN      "\x1b[32m"
-#define ANSI_RED        "\x1b[31m"
+#define ANSI_BOLD "\x1b[1m"
+#define ANSI_CLEAR "\x1b[0m"
+#define ANSI_GREEN "\x1b[32m"
+#define ANSI_RED "\x1b[31m"
 
+MTRand rng;
 uint64_t global_canary;
 
-void canary() {
+void seed(MTRand *rng)
+{
+	int fd = open("/dev/urandom", O_RDONLY);
+	if (fd == -1)
+	{
+		puts("error opening /dev/urandom");
+		exit(1);
+	}
+
+	unsigned long seed;
+	if (read(fd, &seed, sizeof(seed)) != sizeof(seed))
+	{
+		puts("error reading /dev/urandom");
+		exit(1);
+	}
+	close(fd);
+
+	rng = seedRand(seed);
+}
+
+void canary()
+{
 	printf("selecting canary\n");
 }
 
-void game() {
+void game()
+{
 	printf("playing game\n");
 }
 
-void start() {
+void start()
+{
 	char userAction;
-	
+
 	puts("");
 	puts("       .-\"-.");
 	puts("      /  ,~a\\_");
@@ -37,45 +61,58 @@ void start() {
 	puts("");
 	printf("[" ANSI_GREEN "*" ANSI_CLEAR "]: ");
 	scanf(" %c", &userAction);
-	
-	while(1) {
-		if (userAction == 'c') {
+
+	while (1)
+	{
+		if (userAction == 'c')
+		{
 			canary();
 			break;
-		} else if (userAction == 'p') {
+		}
+		else if (userAction == 'p')
+		{
 			game();
 			break;
-		} else {
+		}
+		else
+		{
 			printf("[" ANSI_GREEN "*" ANSI_CLEAR "] invalid action, please try again: ");
-		  while(getchar() != '\n');
+			while (getchar() != '\n')
+				;
 			scanf(" %c", &userAction);
 		}
 	}
 }
 
-void check_canary(uint64_t canary) {
-	if (canary != global_canary) {
+void check_canary(uint64_t canary)
+{
+	if (canary != global_canary)
+	{
 		printf("***** stack smashing detected *****");
 		exit(1);
 	}
 }
 
-void generate_canary() {
+void generate_canary()
+{
 }
 
-void test_rand() {
-	MTRand r = seedRand(1337);
+void test_rand()
+{
 	int i;
-	for (i=0; i < 1000; i++){
-		printf("%ld\n", genRandLong(&r));
+	for (i = 0; i < 1000; i++)
+	{
+		printf("%ld\n", genRandLong(&rng));
 	}
 	return;
 }
 
-int main() {
+int main()
+{
 	gid_t gid = getegid();
 	setresgid(gid, gid, gid);
-	
+
+	seed(&rng);
 	test_rand();
 	// start();
 
